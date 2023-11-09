534cd61946be48b6b6b5c732c642b5c3dd82435e
Ivana Kasikovic <ikasikovic@yahoo.com>
2023-11-09 23:09:59 +0100

    Add minor changes

--- payment-service-provider/crypto/src/test/java/com/project/crypto/CryptoApplicationTests.java
+++ payment-service-provider/crypto/src/test/java/com/project/crypto/CryptoApplicationTests.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 class CryptoApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() {}
 
 }
--- payment-service-provider/paypal/src/test/java/com/project/paypal/PaypalApplicationTests.java
+++ payment-service-provider/paypal/src/test/java/com/project/paypal/PaypalApplicationTests.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 class PaypalApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() {}
 
 }
