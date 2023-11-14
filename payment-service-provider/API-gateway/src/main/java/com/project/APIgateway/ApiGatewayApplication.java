44afaf4094bbaf37a0817e7e7fc39c2d6e614159
Ivana Kasikovic <ikasikovic@yahoo.com>
2023-11-15 00:27:00 +0100

    Add function for paypal flow

--- payment-service-provider/API-gateway/src/main/java/com/project/APIgateway/ApiGatewayApplication.java
+++ payment-service-provider/API-gateway/src/main/java/com/project/APIgateway/ApiGatewayApplication.java
@@ -1,6 +1,5 @@
 package com.project.apigateway;
 
-import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
