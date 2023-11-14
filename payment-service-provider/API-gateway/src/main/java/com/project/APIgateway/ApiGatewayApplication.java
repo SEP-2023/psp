bd94ff74a5ffe00514720214846d9d1b1dfa1874
Ivana Kasikovic <ikasikovic@yahoo.com>
2023-11-14 19:41:34 +0100

    Fix errors

--- payment-service-provider/API-gateway/src/main/java/com/project/APIgateway/ApiGatewayApplication.java
+++ payment-service-provider/API-gateway/src/main/java/com/project/APIgateway/ApiGatewayApplication.java
@@ -4,6 +4,7 @@ import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.web.cors.CorsConfiguration;
@@ -16,6 +17,7 @@ import reactor.netty.http.client.HttpClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableZuulProxy
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
