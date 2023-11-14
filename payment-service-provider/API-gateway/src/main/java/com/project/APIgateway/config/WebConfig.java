9b9891a5ea6f73b2485358eed5a5758c63404d62
Ivana Kasikovic <ikasikovic@yahoo.com>
2023-11-14 21:36:47 +0100

    Delete unneccessary code

--- payment-service-provider/API-gateway/src/main/java/com/project/APIgateway/config/WebConfig.java
+++ payment-service-provider/API-gateway/src/main/java/com/project/APIgateway/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.project.APIgateway.config;
+package com.project.apigateway.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -6,13 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -37,16 +33,3 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
 }
 
 
-
-//@Configuration
-//@EnableWebMvc
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry){
-//        registry.addMapping("/**").allowedOrigins("http://localhost:4200")
-//                .allowedOrigins("*").allowedHeaders("*").allowedMethods("GET","POST", "PUT", "DELETE", "PATCH", "OPTIONS");
-//
-//    }
-//
-//}
