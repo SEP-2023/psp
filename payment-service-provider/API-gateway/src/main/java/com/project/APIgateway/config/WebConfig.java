40b05d1a40dc93127a32787b80976d9f9eac6168
unknown <marijamaki26@gmail.com>
2023-11-14 17:17:21 +0100

    Try

--- payment-service-provider/API-gateway/src/main/java/com/project/APIgateway/config/WebConfig.java
+++ payment-service-provider/API-gateway/src/main/java/com/project/APIgateway/config/WebConfig.java
@@ -1,10 +1,43 @@
-//package com.project.apigateway.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
+package com.project.APIgateway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and().authorizeRequests()
+                .anyRequest().permitAll()
+                .and().csrf().disable();
+    }
+}
+
+
+
 //@Configuration
 //@EnableWebMvc
 //public class WebConfig implements WebMvcConfigurer {
@@ -12,9 +45,8 @@
 //    @Override
 //    public void addCorsMappings(CorsRegistry registry){
 //        registry.addMapping("/**").allowedOrigins("http://localhost:4200")
-//                .allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+//                .allowedOrigins("*").allowedHeaders("*").allowedMethods("GET","POST", "PUT", "DELETE", "PATCH", "OPTIONS");
 //
 //    }
 //
 //}
-//
