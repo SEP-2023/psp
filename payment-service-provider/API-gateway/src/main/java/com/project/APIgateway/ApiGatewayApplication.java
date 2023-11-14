109aef92c081732f17d889ad38a90b4250720cc2
Ivana Kasikovic <ikasikovic@yahoo.com>
2023-11-14 19:41:34 +0100

    Fix errors

--- payment-service-provider/API-gateway/src/main/java/com/project/APIgateway/ApiGatewayApplication.java
+++ payment-service-provider/API-gateway/src/main/java/com/project/APIgateway/ApiGatewayApplication.java
@@ -4,19 +4,7 @@ import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-<<<<<<< HEAD
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-=======
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import reactor.netty.http.client.HttpClient;
->>>>>>> 40b05d1 (Try)
 
 @SpringBootApplication
 @EnableEurekaClient
