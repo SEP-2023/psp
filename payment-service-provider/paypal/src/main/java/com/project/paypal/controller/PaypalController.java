06590259dfcf260ca7e866165471609124888cda
Ivana Kasikovic <ikasikovic@yahoo.com>
2023-11-16 01:14:27 +0100

    Create payment flow

--- payment-service-provider/paypal/src/main/java/com/project/paypal/controller/PaypalController.java
+++ payment-service-provider/paypal/src/main/java/com/project/paypal/controller/PaypalController.java
@@ -4,15 +4,16 @@ import com.paypal.api.payments.Agreement;
 import com.paypal.api.payments.Links;
 import com.paypal.api.payments.Payment;
 import com.paypal.base.rest.PayPalRESTException;
-import com.project.paypal.dto.*;
+import com.project.paypal.dto.ConfirmPaymentDto;
+import com.project.paypal.dto.CreatePaymentDto;
+import com.project.paypal.dto.ExecutionPaymentResultDto;
+import com.project.paypal.dto.RedirectToPaypalDto;
 import com.project.paypal.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
 
 @RestController
 @RequiredArgsConstructor
@@ -47,50 +48,12 @@ public class PaypalController {
             if (executed) {
                 ExecutionPaymentResultDto res = new ExecutionPaymentResultDto();
                 res.setExecuted(true);
-                paymentService.saveSuccessfulTransaction(dto);
                 return new ResponseEntity<>(res, HttpStatus.OK);
-            } else {
-                paymentService.saveFailedTransaction(dto);
-            }
-        } catch (PayPalRESTException e) {
-            System.out.println(e.getMessage());
-        }
-        return new ResponseEntity<ExecutionPaymentResultDto>(HttpStatus.BAD_REQUEST);
-    }
-
-    @PostMapping("/create-subscription")
-    public ResponseEntity<?> createSubscription(@RequestBody CreateSubscriptionDto info) {
-        System.out.println("tu je");
-        try {
-            Agreement agreement = paymentService.createSubscription(info);
-            for (Links link : agreement.getLinks()) {
-                if (link.getRel().equals("approval_url")) {
-                    RedirectToPaypalDto dto = new RedirectToPaypalDto();
-                    dto.setRedirectUrl(link.getHref());
-                    dto.setToken(agreement.getToken());
-                    return new ResponseEntity<>(dto, HttpStatus.OK);
-                }
             }
             return new ResponseEntity<>(HttpStatus.OK);
         } catch (PayPalRESTException e) {
             System.out.println(e.getMessage());
-        } catch (MalformedURLException | UnsupportedEncodingException e) {
-            e.printStackTrace();
         }
         return new ResponseEntity<ExecutionPaymentResultDto>(HttpStatus.BAD_REQUEST);
-    }
-
-    @GetMapping("/execute-subscription/{token}")
-    public ResponseEntity<Boolean> executeSubscription(@PathVariable String token) {
-        try {
-            if (paymentService.executeSubscription(token)) {
-                return new ResponseEntity<>(true, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        } catch (PayPalRESTException e) {
-            e.printStackTrace();
-        }
-        return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
