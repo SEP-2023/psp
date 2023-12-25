68612e9f581c59362d32d56c1be323ea93c1a646
Ivana Jankovic <ivanaj0610@gmail.com>
2023-12-25 21:26:31 +0100

    Reformat

--- payment-service-front/src/app/pages/payment-way-page/payment-way-page.component.ts
+++ payment-service-front/src/app/pages/payment-way-page/payment-way-page.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import {environment} from "../../../environments/environment";
+import { environment } from '../../../environments/environment';
 import { PaypalService } from 'src/app/service/paypal.service';
-import {ActivatedRoute} from "@angular/router";
-import {CryptoService} from "../../service/crypto.service";
-import {SubscriptionService} from "../../service/subscription.service";
-import {BankService} from "../../service/bank.service";
+import { ActivatedRoute } from '@angular/router';
+import { CryptoService } from '../../service/crypto.service';
+import { SubscriptionService } from '../../service/subscription.service';
+import { BankService } from '../../service/bank.service';
 
 @Component({
   selector: 'app-payment-way-page',
   templateUrl: './payment-way-page.component.html',
-  styleUrls: ['./payment-way-page.component.css']
+  styleUrls: ['./payment-way-page.component.css'],
 })
 export class PaymentWayPageComponent implements OnInit {
-
-
-
-  constructor(private paypalService: PaypalService,private cryptoService: CryptoService, private subscriptionService: SubscriptionService, private route: ActivatedRoute, private bankService: BankService) {
+  constructor(
+    private paypalService: PaypalService,
+    private cryptoService: CryptoService,
+    private subscriptionService: SubscriptionService,
+    private route: ActivatedRoute,
+    private bankService: BankService
+  ) {
     this.paypalSubscribed = false;
     this.qrSubscribed = false;
     this.bitcoinSubscribed = false;
@@ -31,32 +34,38 @@ export class PaymentWayPageComponent implements OnInit {
   qrSubscribed!: boolean;
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       this.price = params['price'];
       this.transactionId = params['transactionId'];
       this.agencyId = params['agencyId'];
       console.log('Received parameters:', this.price);
     });
-    this.subscriptionService
-      .getPaymentMethods(this.agencyId)
-      .subscribe(
-        (data) => {
-          console.log(data)
-          this.qrSubscribed = data.some((item: { paymentMethod: string; }) => item.paymentMethod === 'qr');
-          this.paypalSubscribed = data.some((item: { paymentMethod: string; }) => item.paymentMethod === 'paypal');
-          this.cardSubscribed = data.some((item: { paymentMethod: string; }) => item.paymentMethod === 'card');
-          this.bitcoinSubscribed = data.some((item: { paymentMethod: string; }) => item.paymentMethod === 'bitcoin');
-          console.log(this.bitcoinSubscribed)
-        },
-        (error) => {
-          console.log(error);
-          alert('Greska');
-        }
-      );
+    this.subscriptionService.getPaymentMethods(this.agencyId).subscribe(
+      (data) => {
+        console.log(data);
+        this.qrSubscribed = data.some(
+          (item: { paymentMethod: string }) => item.paymentMethod === 'qr'
+        );
+        this.paypalSubscribed = data.some(
+          (item: { paymentMethod: string }) => item.paymentMethod === 'paypal'
+        );
+        this.cardSubscribed = data.some(
+          (item: { paymentMethod: string }) => item.paymentMethod === 'card'
+        );
+        this.bitcoinSubscribed = data.some(
+          (item: { paymentMethod: string }) => item.paymentMethod === 'bitcoin'
+        );
+        console.log(this.bitcoinSubscribed);
+      },
+      (error) => {
+        console.log(error);
+        alert('Greska');
+      }
+    );
   }
 
-  redirect(){
-    console.log('radi')
+  redirect() {
+    console.log('radi');
   }
 
   redirectToBank(qr: boolean) {
@@ -64,7 +73,7 @@ export class PaymentWayPageComponent implements OnInit {
       .getPaymentUrl(this.price, this.transactionId, this.agencyId, qr)
       .subscribe(
         (data) => {
-          console.log(data)
+          console.log(data);
           window.location.href = data.paymentUrl;
         },
         (error) => {
@@ -77,12 +86,12 @@ export class PaymentWayPageComponent implements OnInit {
     window.location.href = `${environment.psp_front_url}/subscription?price=${this.price}&transactionId=${this.transactionId}&agencyId=${this.agencyId}`;
   }
 
-  createPaypalPayment(){
+  createPaypalPayment() {
     this.paypalService
       .createPayment(this.price, this.transactionId, this.agencyId)
       .subscribe(
         (data) => {
-          console.log(data)
+          console.log(data);
           window.location.href = data.redirectUrl;
         },
         (error) => {
@@ -92,12 +101,12 @@ export class PaymentWayPageComponent implements OnInit {
       );
   }
 
-  createCryptoOrder(){
+  createCryptoOrder() {
     this.cryptoService
       .createPayment(this.price, this.transactionId, this.agencyId)
       .subscribe(
         (data) => {
-          console.log(data)
+          console.log(data);
           window.open(data.payment_url);
         },
         (error) => {
