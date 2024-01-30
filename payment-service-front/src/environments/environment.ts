584417985855cfa963213eb183a747da75fe6982
Ivana Jankovic <ivanaj0610@gmail.com>
2024-01-30 18:23:24 +0100

    Update ip address

--- payment-service-front/src/environments/environment.ts
+++ payment-service-front/src/environments/environment.ts
@@ -13,14 +13,14 @@ export const environment = {
   // psp_front_url:"http://localhost:4200",
   // bank_url: "http://localhost:8086/",
   //
-  bank_front_url: 'http://89.216.102.70:4202',
-  paypal_service_url: 'http://89.216.102.70:8000/paypal-service',
-  crypto_service_url: 'http://89.216.102.70:8000/crypto-service',
-  psp_backend: 'http://89.216.102.70:8000/',
-  agency_backend: 'http://89.216.102.70:8085/',
-  agency_front_url: 'http://89.216.102.70:4201',
-  psp_front_url: 'http://89.216.102.70:4200',
-  bank_url: 'http://89.216.102.70:8086/',
+  bank_front_url: 'http://192.168.0.15:4202',
+  paypal_service_url: 'http://192.168.0.15:8000/paypal-service',
+  crypto_service_url: 'http://192.168.0.15:8000/crypto-service',
+  psp_backend: 'http://192.168.0.15:8000/',
+  agency_backend: 'http://192.168.0.15:8085/',
+  agency_front_url: 'http://192.168.0.15:4201',
+  psp_front_url: 'http://192.168.0.15:4200',
+  bank_url: 'http://192.168.0.15:8086/',
 };
 
 /*
